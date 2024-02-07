7d1b32e798681a7564b265134b5486107bfd6060
umarshakeb <umarshakeb@gmail.com>
2024-02-07 23:55:02 +0530

    updated exception.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-from logger import logging
+from src.logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -18,9 +18,9 @@ class CustomException(Exception):
         return self.error_message
     
 
-if __name__ == "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by zero")
-        raise CustomException(e,sys)
+# if __name__ == "__main__":
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by zero")
+#         raise CustomException(e,sys)
