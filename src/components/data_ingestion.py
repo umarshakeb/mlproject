1c4260553f9a2f1763258f1c0573656982b6f023
umarshakeb <umarshakeb@gmail.com>
2024-03-02 23:59:14 +0530

    Updated data_ingestion file

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
@@ -18,6 +19,23 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method")
         try:
-            pass
-        except:
-            pass
+            df = pd.read_csv('notebook/data/StudentsPerformance.csv')
+            logging.info("Reading the dataset from the path")
+
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
+
+            df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
+            
+            logging.info("Train Test split of data initiated")
+            train_set, test_set = train_test_split(df, test_size=0.2, random_state=12)
+            train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
+            test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
+
+            logging.info("Data ingestion completed")
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    obj.initiate_data_ingestion()
