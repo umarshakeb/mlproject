ea04e6ac149d491f7ea78f532e2cad7068a2c247
umarshakeb <umarshakeb@gmail.com>
2024-03-04 23:19:43 +0530

    Making changes to data_transformatio file

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -65,3 +65,7 @@ class DataTransformation:
             return preprocessor
         except Exception as e:
             raise CustomException(e,sys)
+    
+    def initiate_data_transformation(self, train_path, test_path):
+
+        try:
